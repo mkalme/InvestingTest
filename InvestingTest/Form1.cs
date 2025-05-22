381d2a2486f18af2c9088b902c5fb817f0e6749d
mkalme <mikelis.kalme@gmail.com>
2025-05-22 15:03:55 +0300

    Add project files.

---
+++ InvestingTest/Form1.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Diagnostics;
+
+namespace InvestingTest
+{
+    public partial class Form1 : Form
+    {
+        public static double s_p500 = 7;
+
+        public static double netWorth = 0;
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            netWorth += 2000;
+        }
+    }
+}
